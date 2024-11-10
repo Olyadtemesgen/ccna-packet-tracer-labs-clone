ccb11c7091457ea4cfc7215ecbc2f689ed25f78f
Olyadtemesgen <olexyit@gmail.com>
2024-11-10 02:33:38 -0700

    added readme

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
     <NuxtPage />
+    <template>
+    <div>
+        <nav>
+            <PromoBanner />
+            <Header />
+        </nav>
+        <main>
+            <CourseOverview />
+            <div class="sm:w-[55%] flex flex-col justify-center align-middle md:block ml-0 space-y-8 md:ml-28 mt-6">
+                <CertificationsInfo />
+                <LearningObjectives />
+                <ExploreTopics />
+                <CourseContent />
+                <RequirementsAndDescriptions />
+                <FeaturedReview />
+                <CoursesStudentsAlsoBought />
+                <FrequentlyBoughtTogether />
+                <InstructorSection />
+                <CourseReviews />
+                <MoreCoursesSection />
+            </div>
+        </main>
+        <Footer/>
+    </div>
+</template>
+  
   </div>
 </template>
